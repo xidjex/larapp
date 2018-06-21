91d7183ec97aa5195b1e249d80ad72bddfcb29d5
xidjex <xidjex@bk.ru>
2018-06-21 20:24:40 +0300

    Initial commit

---
+++ database/migrations/2018_06_20_203401_create_apartments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->integer('floor');
+            $table->integer('status')->index();
+            $table->integer('user_id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('mount_at')->nullable();
+            $table->timestamp('done_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
